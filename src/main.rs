7c02c150ee219631cbec0e5cb88f2dee19934570
Trangar <victor.koenders@gmail.com>
2020-03-04 11:41:46 +0100

    Added benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![feature(test)]
+#[cfg(test)]
+extern crate test;
+
+extern crate rayon;
+
 mod color;
 mod math;
 mod ray;
@@ -70,6 +76,131 @@ fn main() {
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
     writer.write_image_data(&bytes).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+    
+    fn bench_size_threaded(b: &mut Bencher, width: u32, height: u32) {
+        let mut scene = Scene::new(Color::black());
+        scene.ambient_lights.push(Color::new(0.1, 0.1, 0.1));
+        scene
+            .directional_lights
+            .push((Vector3::new(0.0, 0.0, 1.0), Color::white()));
+        
+        for _ in 0..333 {
+            scene.add(Sphere {
+                center: Vector3::new(11.0, 3.0, 0.0),
+                size: 3.0,
+                color: Color::red(),
+            });
+
+            scene.add(Sphere {
+                center: Vector3::new(9.0, -2.5, -2.5),
+                size: 3.0,
+                color: Color::green(),
+            });
+
+            scene.add(Sphere {
+                center: Vector3::new(10.0, -1.5, 3.5),
+                size: 3.0,
+                color: Color::blue(),
+            });
+        }
+
+        let pixels: Vec<(u32, u32)> = (0..width)
+            .flat_map(|x| (0..height).map(move |y| (x, y)))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            use rayon::prelude::*;
+            let pixels = pixels.clone();
+            let pixels = pixels
+                .into_par_iter()
+                .map(|(x, y)| {
+                    let fx = (x as f32) / 50. - 5.5;
+                    let fy = (y as f32) / 50. - 5.5;
+                    let ray = Ray {
+                        start: Vector3::new(0.0, fx, fy),
+                        direction: Vector3::new(1.0, 0.0, 0.0),
+                    };
+                    calculate_color(&scene, ray, 5)
+                })
+                .collect::<Vec<_>>();
+            pixels
+        });
+
+    }
+
+    #[bench]
+    fn bench_800x600_threaded(b: &mut Bencher) {
+        bench_size_threaded(b, 800, 600);
+    }
+
+    #[bench]
+    fn bench_1920x1200_threaded(b: &mut Bencher) {
+        bench_size_threaded(b, 1920, 1200);
+    }
+
+    fn bench_size(b: &mut Bencher, width: u32, height: u32) {
+        let mut scene = Scene::new(Color::black());
+        scene.ambient_lights.push(Color::new(0.1, 0.1, 0.1));
+        scene
+            .directional_lights
+            .push((Vector3::new(0.0, 0.0, 1.0), Color::white()));
+        
+        scene.add(Sphere {
+            center: Vector3::new(11.0, 3.0, 0.0),
+            size: 3.0,
+            color: Color::red(),
+        });
+
+        scene.add(Sphere {
+            center: Vector3::new(9.0, -2.5, -2.5),
+            size: 3.0,
+            color: Color::green(),
+        });
+
+        scene.add(Sphere {
+            center: Vector3::new(10.0, -1.5, 3.5),
+            size: 3.0,
+            color: Color::blue(),
+        });
+
+        let pixels: Vec<(u32, u32)> = (0..width)
+            .flat_map(|x| (0..height).map(move |y| (x, y)))
+            .collect::<Vec<_>>();
+
+        b.iter(|| {
+            let pixels = pixels.clone();
+            let pixels = pixels
+                .into_iter()
+                .map(|(x, y)| {
+                    let fx = (x as f32) / 50. - 5.5;
+                    let fy = (y as f32) / 50. - 5.5;
+                    let ray = Ray {
+                        start: Vector3::new(0.0, fx, fy),
+                        direction: Vector3::new(1.0, 0.0, 0.0),
+                    };
+                    calculate_color(&scene, ray, 10)
+                })
+                .collect::<Vec<_>>();
+            pixels
+        });
+
+    }
+
+    #[bench]
+    fn bench_800x600(b: &mut Bencher) {
+        bench_size(b, 800, 600);
+    }
+
+    #[bench]
+    fn bench_1920x1200(b: &mut Bencher) {
+        bench_size(b, 1920, 1200);
+    }
 }
 
 fn calculate_color(scene: &Scene, mut ray: Ray, max_bounces: usize) -> Color {
